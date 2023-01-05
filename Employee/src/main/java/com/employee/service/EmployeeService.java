ead1ce4ade98d4b490fe52e3f165f75a0daf8800
premilapatil <31982080+premilapatil@users.noreply.github.com>
2023-01-05 14:33:40 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/employee/service/EmployeeService.java
@@ -0,0 +1,47 @@
+package com.employee.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.employee.entity.Employee;
+
+public interface EmployeeService {
+
+	/***
+	 * Get employee by employee Id
+	 * @param id
+	 * @return
+	 */
+	public Employee getEmployeeById(Integer id);
+
+	/***
+	 * Get all employees data
+	 * @return
+	 */
+	public List<Employee> getAllEmployeesData();
+
+	/***
+	 * Add employee Data
+	 * @param employee
+	 * @return
+	 */
+	public Employee addEmployeeData(Employee employee);
+
+	/***
+	 * Update employee data
+	 * @param employee
+	 * @return
+	 */
+	public Employee updateEmployeeData(Employee employee);
+
+	/***
+	 * Delete employee data by id
+	 * @param id
+	 * @return
+	 */
+	public void deleteEmployeeById(Integer id);
+	
+	
+
+}
